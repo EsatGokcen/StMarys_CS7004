4dc48ba796979bc5e8b43eb342821b25d589b78a
Esat Gokcen <esatgokcen173@gmail.com>
2025-01-30 10:44:49 +0000

    compelted challenge1, week1 - lambda functions

--- week1/challenge1.py
+++ week1/challenge1.py
@@ -2,3 +2,32 @@
 # Challenge asks to implement a simple rule based system
 # using lambda functions
 
+class RuleEngine():
+
+    def __init__(self):
+        self.rules = []
+
+    def add_rule(self, condition, result):
+        self.rules.append((condition, result))
+
+    def apply_rules(self, input_value):
+        for condition, result in self.rules:
+            if condition(input_value):
+                return result
+        return "Default"
+
+
+def main():
+    engine = RuleEngine()
+    engine.add_rule(lambda x: x < 12570, "0% tax")
+    engine.add_rule(lambda x: x > 12570 and x < 50270, "20% tax")
+    engine.add_rule(lambda x: x > 50270 and x < 125140, "40% tax")
+    engine.add_rule(lambda x: x < 125140, "45% tax")
+
+    answer = int(input("What is your annual income? "))
+
+    result = engine.apply_rules(answer)
+    print(result)
+
+if __name__ == '__main__':
+    main()
