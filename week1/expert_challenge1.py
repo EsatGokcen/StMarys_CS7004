473c793e49dab2ae830102c244e80b294b628fb0
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-03 23:01:51 +0000

    initial weather expert system class

---
+++ week1/expert_challenge1.py
@@ -0,0 +1,13 @@
+# Program a weather expert system that has facts as well as rules
+
+class WeatherExpertSystem:
+
+    def __init__(self):
+        self.__facts = set()
+        self.__rules = []
+
+    def add_fact(self):
+        pass
+
+    def add_rule(self):
+        pass
