2b43cb1b4b22460ead44ffbafe3ec84e7347364f
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-06 12:35:39 +0000

    added rest of the plots to matplotlib

--- week2/fuzzy_logic_system/temperature_fuzzy_system.py
+++ week2/fuzzy_logic_system/temperature_fuzzy_system.py
@@ -5,7 +5,7 @@ class TemperatureFuzzySystem:
 
     def __init__(self):
         self.__memberships = {
-            "very cold" : lambda temp: max(0, 1 - abs((temp - 0) / 10)), #max(lower boundry, 1 - abs((temp - peak) / 10))
+            "very cold" : lambda temp: max(0, 1 - abs((temp - 0) / 10)), #max(0, 1 - abs((temp - peak) / 10))
             "cold" : lambda temp: max(0, 1 - abs((temp - 10) / 10)),
             "warm" : lambda temp: max(0, 1 - abs((temp - 20) / 10)),
             "hot" : lambda temp: max(0, 1 - abs((temp - 30) / 10)),
@@ -18,15 +18,27 @@ class TemperatureFuzzySystem:
         x = range(lower_bound, upper_bound + 1)
 
         very_cold_y = []
+        cold_y = []
+        warm_y = []
+        hot_y = []
+        very_hot_y = []
+
         for temp in x:
             very_cold_y.append(self.__memberships["very cold"](temp))
+            cold_y.append(self.__memberships["cold"](temp))
+            warm_y.append(self.__memberships["warm"](temp))
+            hot_y.append(self.__memberships["hot"](temp))
+            very_hot_y.append(self.__memberships["very hot"](temp))
 
         # MatPlotLib Graph
-        plt.plot(x, very_cold_y)
+        plt.plot(x, very_cold_y, very_cold_ylabel="Very Cold", color='b')
+        plt.plot(x, cold_y, label="Cold", color='g')
+        plt.plot(x, warm_y, label="Warm", color='o')
+        plt.plot(x, hot_y, label="Hot", color='r')
+        plt.plot(x, very_hot_y, label="Very Hot", color='p')
         plt.title("Membership Functions for Linguistic Terms")
         plt.xlabel("Temperature")
         plt.ylabel("Membership Degree")
         plt.legend(["Very Cold", "Cold", "Warm", "Hot", "Very Hot"])
-        plt.plot(x, very_cold_y, label='Line 1')
         plt.legend()
         plt.show()
