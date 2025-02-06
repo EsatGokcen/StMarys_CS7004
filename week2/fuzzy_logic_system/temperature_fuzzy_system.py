c68fafe53f76d62441dd2a452b76e93b43173ed8
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-06 12:43:23 +0000

    minor tweaks and comments

--- week2/fuzzy_logic_system/temperature_fuzzy_system.py
+++ week2/fuzzy_logic_system/temperature_fuzzy_system.py
@@ -17,12 +17,13 @@ class TemperatureFuzzySystem:
         upper_bound = temp_range[1]
         x = range(lower_bound, upper_bound + 1)
 
-        very_cold_y = []
+        very_cold_y = [] # you can use the map() function for cleaner code
         cold_y = []
         warm_y = []
         hot_y = []
         very_hot_y = []
 
+        # Run lambda functions on temp range for each y value
         for temp in x:
             very_cold_y.append(self.__memberships["very cold"](temp))
             cold_y.append(self.__memberships["cold"](temp))
@@ -31,11 +32,11 @@ class TemperatureFuzzySystem:
             very_hot_y.append(self.__memberships["very hot"](temp))
 
         # MatPlotLib Graph
-        plt.plot(x, very_cold_y, very_cold_ylabel="Very Cold", color='b')
+        plt.plot(x, very_cold_y, label="Very Cold", color='b')
         plt.plot(x, cold_y, label="Cold", color='g')
-        plt.plot(x, warm_y, label="Warm", color='o')
+        plt.plot(x, warm_y, label="Warm", color='orange')
         plt.plot(x, hot_y, label="Hot", color='r')
-        plt.plot(x, very_hot_y, label="Very Hot", color='p')
+        plt.plot(x, very_hot_y, label="Very Hot", color='purple')
         plt.title("Membership Functions for Linguistic Terms")
         plt.xlabel("Temperature")
         plt.ylabel("Membership Degree")
