00fbf03a5b406b412b9684df10145036109815e9
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-13 12:14:56 +0000

    initialise population method and new attributes for string genetic algorithm

--- week3/string_genetic_algorithm.py
+++ week3/string_genetic_algorithm.py
@@ -1,7 +1,18 @@
+import random
+import string
+
+
 class StringGeneticAlgorithm:
 
-    def __init__(self):
-        self.word = "hello"
+    def __init__(self, target: str = "hello", population_size: int = 100):
+        self.__target = target
+        self.__population = []
+        self.__population_size = population_size
+
+    def initialise_population(self):
+        for _ in range(self.__population_size):
+            random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(len(self.__target)))
+            self.__population.append(random_string)
 
     def constructor(self):
         pass
