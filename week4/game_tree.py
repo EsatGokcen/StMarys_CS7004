7e2eb085ddaf1b2b783f6eaf75e3a8e7ad9f7bf2
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-20 11:25:20 +0000

    tested the code, it works

--- week4/game_tree.py
+++ week4/game_tree.py
@@ -1,22 +1,24 @@
 class TicTacToe:
 
     def __init__(self):
-        self.__board = [' ' for _ in range(9)]
+        self.__board = []
+        for _ in range(9):
+            self.__board.append(' ')
         self.__running = True
 
     def display_board(self):
         # display the board as a 3 by 3 grid
-        print(f"\n{self.__board[0][1][2]}")
-        print(f"\n{self.__board[3][4][5]}")
-        print(f"\n{self.__board[6][7][8]}")
+        print(f"\n{self.__board[0], self.__board[1], self.__board[2]}")
+        print(f"\n{self.__board[3], self.__board[4], self.__board[5]}")
+        print(f"\n{self.__board[6], self.__board[7], self.__board[8]}")
 
     def player_move(self):
         position = (int(input("Enter your move (1-9): ")) - 1)
         # if position invalid, ask again
         if self.__board[position] != ' ':
-            print("Position already occupied, please select again or press q to quit")
+            print("Position already occupied, please select again or press 0 to quit")
             position = (int(input("Enter your move (1-9): ")) - 1)
-        elif position == 'q':
+        elif position == -1:
             print("Thanks for playing, goodbye!")
             self.__running = False
         else:
