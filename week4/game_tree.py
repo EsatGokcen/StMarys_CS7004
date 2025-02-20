f95a20c37d8a6157539733f946b18c17f2b3b07e
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-20 13:21:14 +0000

    added minimax base case

--- week4/game_tree.py
+++ week4/game_tree.py
@@ -12,8 +12,13 @@ class TicTacToe:
         print(f"{self.__board[3]}_|_{self.__board[4]}_|_{self.__board[5]}")
         print(f"{self.__board[6]} | {self.__board[7]} | {self.__board[8]}")
 
-    def minimax(self, switch: bool) -> int:
-        pass
+    def minimax(self, is_max: bool) -> int:
+        # Base Case
+        if self.win('O'): return 1
+        if self.win('X'): return -1
+        if self.draw(): return 0
+
+        # Recursive Case
 
     def player_move(self):
         position = (int(input("Enter your move (1-9): ")) - 1)
@@ -28,6 +33,7 @@ class TicTacToe:
             self.__board[position] = 'X'
 
     def computer_move(self):
+        best_move = None
         best_score = float('inf') #set this to infinity
         for index in range(len(self.__board)):
             if self.__board[index] == " ":
@@ -38,6 +44,9 @@ class TicTacToe:
                 if score < best_score:
                     best_score = score
                     best_move = index
+
+        if best_move is not None:
+            self.__board[best_move] = 'O'
 
     def game_over(self):
         if self.win():
@@ -50,7 +59,7 @@ class TicTacToe:
             pass
 
 
-    def win(self):
+    def win(self, player: str):
         conditions = [0,1,2], [0,3,6], [0,4,8], [3,4,5], [1,4,7], [2,4,6], [6,7,8], [2,5,8]
         for condition in conditions:
             for i in condition:
@@ -67,6 +76,7 @@ class TicTacToe:
         while self.__running:
             self.display_board()
             self.player_move()
+            self.computer_move()
 
 if __name__ == '__main__':
     game = TicTacToe()
