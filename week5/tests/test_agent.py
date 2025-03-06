71c51b66d3038f350d455f6b40b5fbf740b93e18
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-06 00:00:49 +0000

    added more unittest methods for agent and tested

--- week5/tests/test_agent.py
+++ week5/tests/test_agent.py
@@ -29,5 +29,28 @@ class TestAgent(unittest.TestCase):
 
         self.assertEqual(agent.get_location(), new_location)
 
+    def test_location_attributes(self):
+        """
+        Test location attributes for Agent objects.
+        """
+        location = Location(3, 4)
+        agent = Agent(location)
+
+        self.assertEqual(agent.get_location().get_x(), 3)
+        self.assertEqual(agent.get_location().get_y(), 4)
+
+    def test_location_setters(self):
+        """
+        Test location setters for Agent objects.
+        """
+        location = Location(3, 4)
+        agent = Agent(location)
+
+        agent.get_location().set_x(5)
+        agent.get_location().set_y(6)
+
+        self.assertEqual(agent.get_location().get_x(), 5)
+        self.assertEqual(agent.get_location().get_y(), 6)
+
 if __name__ == '__main__':
     unittest.main()
