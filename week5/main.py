f7a640b59c6aa5ff636f96258a95a27f017ed61e
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-12 15:29:22 +0000

    created simulator object in main

--- week5/main.py
+++ week5/main.py
@@ -1,1 +1,7 @@
-# Intelligent Agent project for week 5
+# Intelligent Agent project for week 5 + 6
+
+from week5.controller.simulator import Simulator
+
+if __name__ == "__main__":
+    simulation = Simulator()
+    simulation.run()
