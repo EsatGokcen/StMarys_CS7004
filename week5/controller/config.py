1016406e7d149b6bbecbb7f82a51c8b356ca9dfb
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-12 12:12:04 +0000

    initial config file blueprint for ai simulation

---
+++ week5/controller/config.py
@@ -0,0 +1,5 @@
+# Use the above template for the configuration file to create a configuration file with the following constants:
+# simulation name,
+# ocean size (number of rows/columns in a symmetrical grid),
+# colours (string values e.g. "red") for each agent.
+# breeding probability (floats between 0 and 1) for each agent.
