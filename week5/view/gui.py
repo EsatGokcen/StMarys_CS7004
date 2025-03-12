c293886800f0e0f0bb750d541d246b1d5a84f3c9
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-12 15:07:03 +0000

    initial gui constructor

--- week5/view/gui.py
+++ week5/view/gui.py
@@ -21,3 +21,21 @@ class Gui(tk.Tk):
         __legend_panel (tk.Frame): The legend panel displaying agent types and their counts.
         __closed (bool): Flag indicating whether the GUI window is closed.
     """
+
+    def __init__(self, environment: Environment, agent_colours: dict):
+        """
+        Initialise the GUI with the given environment and agent colours.
+
+        Args:
+            environment (Environment): The environment instance to visualise.
+            agent_colours (dict): A dictionary mapping agent classes to their corresponding colors.
+        """
+        super().__init__()
+        self.__environment = environment
+        self.__agent_colours = agent_colours
+        self.__legend_panel = None
+        self.__closed = False
+
+        self.__init_gui()
+        self.__init_info()
+        self.__init_world()
