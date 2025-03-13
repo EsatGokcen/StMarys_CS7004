767cf798c813e9a403bcc8ab07b342e4caeb1ebb
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 12:41:43 +0000

    completed particle attributes and used encapsulation

--- week7/pso/particle.py
+++ week7/pso/particle.py
@@ -1,25 +1,18 @@
-"""
-    def __init__(self, dimensions: int, bounds: str, position: list[float], velocity: list[float]):
-        self.dimensions = dimensions
-        self.bounds = bounds
-        self.position = position
-        self.velocity = velocity
-        self.objective_function()
-"""
 import random
+import copy
 
 class Particle:
 
     def __init__(self, bounds: list[tuple[int, int]], objective_function, initial_position: list[float] = None):
-        self.dimensions = len(bounds)
-        self.bounds = bounds
-        self.objective_function = objective_function
+        self.__dimensions = len(bounds)
+        self.__bounds = bounds
+        self.__objective_function = objective_function
         if initial_position:
-            self.position = initial_position
+            self.__position = initial_position
         else:
-            self.position = [random.randint(bound[0], bound[1]) for bound in self.bounds]
-        self.velocity = 0
-
+            self.__position = [random.randint(bound[0], bound[1]) for bound in self.__bounds]
+        self.__velocity = 0
+        self.__p_best_position = copy.deepcopy(self.__position)
 
     def objective_function(self):
         pass
