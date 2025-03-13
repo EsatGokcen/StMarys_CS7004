2a130a9a71ced03b722f4f2dc2be04bb6850f1cd
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 11:28:59 +0000

    testing different attribute implementation

--- week7/pso/particle.py
+++ week7/pso/particle.py
@@ -1,11 +1,20 @@
-class Particle:
-
+"""
     def __init__(self, dimensions: int, bounds: str, position: list[float], velocity: list[float]):
         self.dimensions = dimensions
         self.bounds = bounds
         self.position = position
         self.velocity = velocity
         self.objective_function()
+"""
+
+class Particle:
+
+    def __init__(self, objective_function, bounds_for_dimensions: list[tuple[str, int, int]] = [["x", 0, 4], ["y", 0, 4]], initial_position: list[float] = [0.0, 0.0]):
+        self.dimensions = bounds_for_dimensions[0][0], bounds_for_dimensions[1][0]
+        self.bounds = bounds_for_dimensions[0][1], bounds_for_dimensions[0][2], bounds_for_dimensions[1][1], bounds_for_dimensions[1][2]
+        self.objective_function = objective_function
+        self.position = initial_position
+
 
     def objective_function(self):
         pass
