45489bdcf9278b110d740c321114a2fc5b699783
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-19 14:53:22 +0000

    completed initial agent class

--- week5and6/model/agent.py
+++ week5and6/model/agent.py
@@ -5,35 +5,14 @@ from week5and6.model.ocean import Ocean
 
 class Agent(ABC):
     def __init__(self, location: Location) -> None:
-        """
-        Constructor for the Agent class.
-
-        Parameters:
-        - location: A Location object representing the agent's initial location.
-        """
         self.__location = location
 
     def set_location(self, location: Location) -> None:
-        """
-        Set the location of the agent.
-
-        Parameters:
-        - location: A Location object representing the new location of the agent.
-        """
         self.__location = location
 
     def get_location(self) -> Location:
-        """
-        Get the current location of the agent.
-
-        Returns:
-        A Location object representing the agent's current location.
-        """
         return self.__location
 
     @abstractmethod
     def act(self, ocean: Ocean):
-        """
-        The agent performs some actions.
-        """
         pass
