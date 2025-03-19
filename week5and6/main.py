e9eb3c9b3b4ad360fce7c0a390468bb7e945cc13
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-19 14:51:13 +0000

    fixed main import

--- week5and6/main.py
+++ week5and6/main.py
@@ -1,6 +1,6 @@
 # Intelligent Agent project for week 5 + 6
 
-from week5.controller.simulator import Simulator
+from week5and6.controller.simulator import Simulator
 
 if __name__ == "__main__":
     simulation = Simulator()
