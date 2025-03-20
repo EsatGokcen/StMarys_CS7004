c80d0dc4c376ecb0da3f79369f963e132f5ea839
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-20 12:19:57 +0000

    initial bayesian network class and property methods for node class

---
+++ week8/bayesian_network/bayesian_network.py
@@ -0,0 +1,4 @@
+class BayesianNetwork:
+
+    def __init__(self):
+        pass
--- week8/bayesian_network/node.py
+++ week8/bayesian_network/node.py
@@ -2,22 +2,25 @@ from typing import Self
 
 class Node:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.__name = name
         self.__parents = []
         self.__cpt = {}
 
+    @property
     def get_name(self) -> str:
         return self.__name
 
+    @property
     def get_parents(self) -> list[Self]:
         return self.__parents
 
+    @property
     def get_cpt(self) -> dict[bool | float]:
         return self.__cpt
 
-    def add_parent(self, parent_node: Self):
+    def add_parent(self, parent_node: Self) -> None:
         self.__parents.append(parent_node)
 
-    def remove_parent(self, parent_node: Self):
+    def remove_parent(self, parent_node: Self) -> None:
         self.__parents.remove(parent_node)
