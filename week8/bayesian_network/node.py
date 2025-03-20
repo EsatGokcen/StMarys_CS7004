fb27a82b67fafa13e87ebef65dd404f7288ae892
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-20 10:59:16 +0000

    new add and remove parent methods with the Self type casting for Node class

--- week8/bayesian_network/node.py
+++ week8/bayesian_network/node.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 class Node:
 
     def __init__(self, name: str):
@@ -8,8 +10,14 @@ class Node:
     def get_name(self) -> str:
         return self.__name
 
-    def get_parents(self) -> list:
+    def get_parents(self) -> list[Self]:
         return self.__parents
 
-    def get_cpt(self) -> dict:
+    def get_cpt(self) -> dict[bool | float]:
         return self.__cpt
+
+    def add_parent(self, parent_node: Self):
+        self.__parents.append(parent_node)
+
+    def remove_parent(self, parent_node: Self):
+        self.__parents.remove(parent_node)
