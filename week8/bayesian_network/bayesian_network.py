be1aa4916a3f2762579b94aa9f6e39c730f0b7e3
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-20 12:30:31 +0000

    new add and remove edge methods and a get nodes property for bayesian network class

--- week8/bayesian_network/bayesian_network.py
+++ week8/bayesian_network/bayesian_network.py
@@ -5,8 +5,18 @@ class BayesianNetwork:
     def __init__(self):
         self.__nodes = {}
 
-    def add_node(self, node: Node):
+    @property
+    def get_nodes(self) -> dict[Node]:
+        return self.__nodes
+
+    def add_node(self, node: Node) -> None:
         self.__nodes.update({node.get_name(), node})
 
-    def remove_node(self, node_name: str):
+    def remove_node(self, node_name: str) -> None:
         del self.__nodes[node_name]
+
+    def add_edge(self, parent_node: Node, child_node: Node) -> None:
+        child_node.add_parent(parent_node)
+
+    def remove_edge(self, parent_node: Node, child_node: Node) -> None:
+        child_node.remove_parent(parent_node)
