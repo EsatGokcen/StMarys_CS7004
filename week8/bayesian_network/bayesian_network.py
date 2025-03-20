fd105f1f3bb14b174699ce55d9ef73564a2d68ef
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-20 12:25:18 +0000

    new add and remove node methods for bayesian network class

--- week8/bayesian_network/bayesian_network.py
+++ week8/bayesian_network/bayesian_network.py
@@ -1,4 +1,12 @@
+from week8.bayesian_network.node import Node
+
 class BayesianNetwork:
 
     def __init__(self):
-        pass
+        self.__nodes = {}
+
+    def add_node(self, node: Node):
+        self.__nodes.update({node.get_name(), node})
+
+    def remove_node(self, node_name: str):
+        del self.__nodes[node_name]
