860870f9d79c67c42d0f079ba51b4792f00c4af8
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-20 11:06:23 +0000

    added unittest case for node class

---
+++ week8/unittests/test_node.py
@@ -0,0 +1,11 @@
+import unittest
+from week8.bayesian_network.node import Node
+
+class TestNode(unittest.TestCase):
+
+    def test_add_parent(self):
+        child_node = Node("child node")
+        parent_node = Node("parent node")
+        child_node.add_parent(parent_node)
+
+        TestNode.assertEqual(self, child_node.get_parents()[0], parent_node, "Parent node not in child node as expected.")
