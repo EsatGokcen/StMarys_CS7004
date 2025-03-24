93b09705b3898f88aad058771635f405061e5a5a
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-24 17:59:55 +0000

    removed some unnecesary comments

--- week5and6/model/location.py
+++ week5and6/model/location.py
@@ -8,50 +8,19 @@ class Location:
     """
 
     def __init__(self, x: int, y: int) -> None:
-        """
-        Initialises a Location object with given x and y coordinates.
-
-        Args:
-            x (int): The x-coordinate of the location.
-            y (int): The y-coordinate of the location.
-        """
         self.__x = x
         self.__y = y
 
     def get_x(self) -> int:
-        """
-        Gets the x-coordinate of the location.
-
-        Returns:
-            int: The x-coordinate.
-        """
         return self.__x
 
     def get_y(self) -> int:
-        """
-        Gets the y-coordinate of the location.
-
-        Returns:
-            int: The y-coordinate.
-        """
         return self.__y
 
     def set_x(self, new_x: int) -> None:
-        """
-        Sets the x-coordinate of the location.
-
-        Args:
-            new_x (int): The new x-coordinate.
-        """
         self.__x = new_x
 
     def set_y(self, new_y: int) -> None:
-        """
-        Sets the y-coordinate of the location.
-
-        Args:
-            new_y (int): The new y-coordinate.
-        """
         self.__y = new_y
 
     def equals(self, other_location: 'Location') -> bool:
